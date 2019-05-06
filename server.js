b4fedd0992ad132e7c74a1dbc774a0da4903bf25
Rahman <rachman.sd@gmail.com>
2019-05-06 19:30:32 +0700

    create server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const bodyparser = require('body-parser')
+const cors = require('cors')
+
+
+
+// start
+const app = express()
+
+app.use(cors())
+
+// before body parse
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({ extended: true }))
+
+// after body parse
+
+
+const port = process.env.PORT || 5000
+
+app.listen(port)
+
+// eslint-disable-next-line
+console.info('application serve on port ' + port)
