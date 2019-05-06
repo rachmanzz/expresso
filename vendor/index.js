7b5583593069c4613f37525d63f0bd4fa079c23c
Rahman <rachman.sd@gmail.com>
2019-05-06 20:03:51 +0700

    create file global vendor

---
+++ vendor/index.js
@@ -0,0 +1,10 @@
+global.module = module => {
+    if (/^[a-z]+::[\w]+/.test(module)) {
+        // required vendor module
+        const arr_data = module.match(/(^[a-z]+)::([\w\W\d\D]+)$/)
+        if (arr_data[1] === 'vendor') return require(__dirname + '/modules/' + arr_data[2])
+        else if (arr_data[1] === 'app') return require(__dirname + '../app/' + arr_data[2])
+        else if (arr_data[1] === 'middleware') return require(__dirname + '../app/middleware/' + arr_data[2])
+        else { console.error('invalid module path'); return null }
+    } else return require(module)
+}
