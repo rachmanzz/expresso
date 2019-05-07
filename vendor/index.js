a8704e6ae00f8c879d6d430fe4ee293124f80c8c
Rahman <rachman.sd@gmail.com>
2019-05-07 19:33:36 +0700

    update module vendor

--- vendor/index.js
+++ vendor/index.js
@@ -5,6 +5,8 @@ global._module = module => {
         if (arr_data[1] === 'vendor') return require('./modules/' + arr_data[2])
         else if (arr_data[1] === 'app') return require('../app/' + arr_data[2])
         else if (arr_data[1] === 'middleware') return require('../app/middleware/' + arr_data[2])
+        else if (arr_data[1] === 'database') return require('../database/' + arr_data[2])
+        else if (arr_data[1] === 'config') return require('../config/' + arr_data[2])
         else { console.error('invalid module path'); return null }
     } else return require(module)
 }
